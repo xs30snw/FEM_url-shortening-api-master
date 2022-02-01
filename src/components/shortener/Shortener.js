8747579398d6276be4b08f506ec063266a22c3b2
xs30snw <e4n22s@gmail.com>
2022-02-01 17:01:25 +0500

    Better handling of fetch errors

--- src/components/shortener/Shortener.js
+++ src/components/shortener/Shortener.js
@@ -17,31 +17,42 @@ export class Shortener extends React.Component {
         const shortenInput = document.getElementById('shortenInput');
         
         fetch('https://api.shrtco.de/v2/shorten?url=' + shortenInput.value)
-            .then(response => response.json())
+            .then(response => {
+                // Handle response status beyond 200-299
+                if (!response.ok) {
+                    throw new Error('The fetch response is not ok');
+                };
+                response.json();
+            })
             .then(data => {
                 // Http input validation heuristic
                 const output = data?.result?.full_short_link;
                 if (!output) {
-                    throw 'Problems with shortening';
+                    throw new Error('Problems with shortening');
                 };
 
-                // Limit shortened items to 3
+                // Limit the number of Shortener items to 3
                 if (items.length >= 3) {
                     items.pop();
                 };
 
+                // Add next shortened link to Shortener item list
                 this.setState({
                     items: items.concat([{
                         input: shortenInput.value,
                         output: data?.result?.full_short_link,
                     }]),
                 })
+
+                // Hide error label, is successful
                 const labelError = document.getElementById('labelError');
                 labelError.classList.add('hide');
             })
-            .catch(() => {
+            .catch(error => {
+                // Show error label, is erroneus
                 const labelError = document.getElementById('labelError');
                 labelError.classList.remove('hide');
+                console.log(error);
             });
     }
 
