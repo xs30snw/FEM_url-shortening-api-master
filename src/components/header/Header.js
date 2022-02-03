403df0df2fdab67b9cb9a2641387ed5daf73002e
xs30snw <e4n22s@gmail.com>
2022-02-03 20:21:42 +0500

    Better styles, improved Header, transitions

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,50 +5,61 @@ import './Header.css';
 
 function Header() {
     const [navOpen, setNavOpen] = useState(false);
-
+    
     function handleClick() {
-        setNavOpen(!navOpen);
+        setNavOpen(!navOpen);        
+    }
+
+    let timer;
+
+    function closeNavAfter(time) {
+        timer = window.setTimeout(
+            () => setNavOpen(false),
+            time
+        );
+    }
+
+    function resetNavClosing() {
+        clearTimeout(timer);
     }
 
     return (
         <header 
             className="header container"
-            onMouseLeave={() => {
-                if (navOpen) {
-                    setNavOpen(false);
-                }
-            }}    
+            onMouseLeave={() => closeNavAfter(500)}
+            onMouseOver={() => resetNavClosing()}  
         >
-            <img src={ShortlyLogo} alt="Shortly logo" />
+            <img className="header__logo" src={ShortlyLogo} alt="Shortly logo" />
 
-            <nav className="navDesktopMenu">
+            <nav className="header__nav-desktop">
                 <ul>
-                    <li><a href="#root" className="link">Features</a></li>
-                    <li><a href="#root" className="link">Pricing</a></li>
-                    <li><a href="#root" className="link">Resources</a></li>
+                    <li><a href="#root" className="btn-secondary">Features</a></li>
+                    <li><a href="#root" className="btn-secondary">Pricing</a></li>
+                    <li><a href="#root" className="btn-secondary">Resources</a></li>
                 </ul>
                 <ul>
-                    <li><a href="#root" className="link">Login</a></li>
-                    <li><button className="button1">Sign up</button></li>
+                    <li><a href="#root" className="btn-secondary">Login</a></li>
+                    <li><button className="btn-primary">Sign Up</button></li>
                 </ul>
             </nav>
 
-            <nav className={"navMobileMenu " + (navOpen ? "show" : "hide" )}>
+            <nav className={"header__nav-mobile " + (navOpen ? "" : "hide" )}>
                 <ul>
-                    <li><a href="#root" className="link">Features</a></li>
-                    <li><a href="#root" className="link">Pricing</a></li>
-                    <li><a href="#root" className="link">Resources</a></li>
+                    <li><a href="#root" className="btn-secondary btn-secondary--mobile">Features</a></li>
+                    <li><a href="#root" className="btn-secondary btn-secondary--mobile">Pricing</a></li>
+                    <li><a href="#root" className="btn-secondary btn-secondary--mobile">Resources</a></li>
                     <hr />
-                    <li><a href="#root" className="link">Login</a></li>
-                    <li><button className="button1">Sign up</button></li>
+                    <li><a href="#root" className="btn-secondary btn-secondary--mobile">Login</a></li>
+                    <li><button className="btn-primary btn-primary--mobile">Sign up</button></li>
                 </ul>
             </nav>
 
-            <i className="las la-bars navMobileButton"
+            <i className="las la-bars header__btn-mobile-menu"
                onClick={() => handleClick()}
             ></i>
+        
         </header>
     )
 }
 
-export default Header
+export default Header;
