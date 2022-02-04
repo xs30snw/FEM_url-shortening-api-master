35741ac165555735cf1dc698d0ab257981ce0256
xs30snw <e4n22s@gmail.com>
2022-02-04 20:12:13 +0500

    Refactored and improved Boost, fixed mobile menu

--- src/components/boost/Boost.js
+++ src/components/boost/Boost.js
@@ -3,10 +3,12 @@ import "./Boost.css";
 function Boost() {
     return (
         <section className="boost">
-            <h3>Boost your links today</h3>
-            <a href="#root"><button className="button1">Get Started</button></a>
+            <h3 className="boost__title">Boost your links today</h3>
+            <a href="#root">
+                <button className="boost__btn btn-primary">Get Started</button>
+            </a>
         </section>
     )
 }
 
-export default Boost
+export default Boost;
