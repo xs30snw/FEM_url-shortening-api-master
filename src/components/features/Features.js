4ea3be30041a73b2e7f0d0781ce3b3fa370ebfcc
xs30snw <e4n22s@gmail.com>
2022-02-04 19:37:37 +0500

    Refactored and improved Features

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,49 +1,55 @@
-import './Features.css';
 import iRecognition from './icon-brand-recognition.svg';
-import iRecords from './icon-detailed-records.svg';
-import iCustomize from './icon-fully-customizable.svg';
+import iRecords     from './icon-detailed-records.svg';
+import iCustomize   from './icon-fully-customizable.svg';
+
+import './Features.css';
 
 function Features() {
     return (
-        <section className="features">
-            <div className="container">
-                <div className="featuresHeader">
-                    <h3>Advanced statistics</h3>
-                    <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+        <section className='features'>
+            <div className='container'>
+                <div className='features__header'>
+                    <h3 className='features__header__title'>Advanced Statistics</h3>
+                    <p className='features__header__desc'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
                 </div>
-                <div className="featuresArticles">
-                    <article>
-                        <div className="featuresIcon">
-                            <img src={iRecognition} alt="" />
+
+                <div className='features__cards'>
+
+                    <div className='features__card'>
+                        <div className='features__card__icon'>
+                            <img src={iRecognition} alt='' />
                         </div>
-                        <h4>Brand Recognition</h4>
-                        <p>
+                        <h4 className='features__card__title'>Brand Recognition</h4>
+                        <p className='features__card__desc'>
                             Boost your brand recognition with each click.
                             Generic links don't mean a thing.
                             Branded links help instil confidence in your content.
                         </p>
-                    </article>
-                    <article>
-                        <div className="featuresIcon">
-                            <img src={iRecords} alt="" />
+                    </div>
+
+                    <div className='features__card'>
+                        <div className='features__card__icon'>
+                            <img src={iRecords} alt='' />
                         </div>
-                        <h4>Detailed Records</h4>
-                        <p>
+                        <h4 className='features__card__title'>Detailed Records</h4>
+                        <p className='features__card__desc'>
                             Gain insights into who is clicking your links.
                             Knowing when and where people engage with your content helps inform better decisions.
                         </p>
-                    </article>
-                    <article>
-                        <div className="featuresIcon">
-                            <img src={iCustomize} alt="" />
+                    </div>
+
+                    <div className='features__card'>
+                        <div className='features__card__icon'>
+                            <img src={iCustomize} alt='' />
                         </div>
-                        <h4>Fully Customizible</h4>
-                        <p>
+                        <h4 className='features__card__title'>Fully Customizible</h4>
+                        <p className='features__card__desc'>
                             Improve brand awareness and content discoverability through customizible links,
                             supercharging audience engagement.
                         </p>
-                    </article>
-                </div>
+                    </div>
+
+                </div>                
             </div>
         </section>
     )
